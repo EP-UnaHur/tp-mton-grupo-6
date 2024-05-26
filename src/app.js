6c101d6e28b3d1bdb745030c80a1bf4e969fdb9b
MaauroSaavedra <mauro.saavedra@estudiantes.unahur.edu.ar>
2024-05-26 18:19:34 -0300

    app en linea

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-console.log("hola mundo")
+const express = require("express");
+
+const app = express();
+
+app.get('/cursos', (req, res)=>{
+    res.status(200).json({
+        message: 'hola'
+    })
+})
+
+app.listen(3000, ()=>{
+    console.log('la app esta corriendo en el puerto 3000')
+})
