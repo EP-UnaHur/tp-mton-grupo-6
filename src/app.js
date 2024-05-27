a8a34163d6bb857cd0985f78d468baffd2347196
MaauroSaavedra <mauro.saavedra@estudiantes.unahur.edu.ar>
2024-05-27 16:54:56 -0300

    creo los endpoinds de carrera

--- src/app.js
+++ src/app.js
@@ -1,42 +1,49 @@
 const express = require("express")
-const _ = require('lodash')
 const data = require("../data/data.json")
+const db = require('./db/models')
 
+const _ = require('lodash');
+const { where } = require("sequelize");
 const app = express();
 app.use(express.json())
 
-app.get('/carreras', (req, res)=>{
-    res.status(200).json(data)
+app.get('/carreras', async (req, res)=>{
+    const carreras = await db.Carrera.findAll({});
+    res.status(200).json(carreras)
 })
 
-app.get('/carreras/:id', (req, res)=>{
+app.get('/carreras/:id', async (req, res)=>{
     const id = req.params.id
-    const carrera = data.find(e => e.id == id)
+    const carrera = await db.Carrera.findOne(
+        {
+            where: {id},
+            attributes:['id', 'nombre', 'grado', 'universidad']
+        })
     if (carrera) 
         res.status(200).json(carrera)
-    else res.status(404).json({error: 'el id no existe'})
+    else res.status(404).json({error: `el id ${id} no existe`})
 })
 
-app.delete('/carreras/:id', (req, res)=>{
-    const id = req.params.id;
-    const idx = data.findIndex(e => e.id == id)
-    if (idx >= 0) {
-    const removed = data.splice(idx, 1)
-    res.status(202).json({
-        mensaje: 'la carrera fue eliminada con exito',
-        carrera: removed
-    })
-    } else res.status(404).json({error: 'el id no existe'})
+app.post('/carreras', async (req, res)=>{
+    try {
+        const carrera = req.body
+        const newCarrera = await db.Carrera.create(carrera)
+        res.status(201).json(newCarrera)
+    } catch (err) {    
+        res.status(400).json(err.message)
+    }
 })
 
-app.post('/carreras', (req, res)=>{
-    const carrera = req.body
-    let id = 0
-    if (data.length) 
-        id = _.max(data.map(e=>e.id))
-    const aGrabar = {id: id + 1, ...carrera}
-    data.push(aGrabar)
-    res.status(201).json(aGrabar)
+app.post('/carreras/:id/materia', async (req, res)=>{
+    const idCarrera = req.params.id;
+    const carrera = await db.Carrera.findByPk(idCarrera)
+    if (carrera) {
+        const materia = req.body
+        const updateCarrera = await db.Materia.create({carreraId:carrera.id, ...materia})
+        res.status(201).json(updateCarrera)
+    } else {
+        res.status(404).json({error: `El id ${idCarrera} no existe.`})
+    }
 })
 
 app.put('/carreras/:id', (req, res)=>{
@@ -46,9 +53,33 @@ app.put('/carreras/:id', (req, res)=>{
         data[idx] = {id: Number(id), ...req.body}
         res.status(200).json(data[idx])
     } else
-        res.status(404).json({error: 'el id no existe'})
+        res.status(404).json({error: `el id ${id} no existe`})
 })
 
-app.listen(3000, ()=>{
-    console.log('la app esta corriendo en el puerto 3000')
+app.get('/materias', async (req, res)=>{
+    const materias = await db.Materia.findAll({});
+    res.status(200).json(materias)
+})
+
+app.listen(3000, async ()=>{
+    console.log('la app esta corriendo en el puerto 3000');
+    try {
+        await db.sequelize.authenticate()
+        await db.sequelize.sync({force:true});
+   
+        db.Carrera.create({
+            nombre: 'Informatica',
+            grado: '3',
+            universidad: 'UNAHUR'
+        })
+   
+        db.Carrera.create({
+            nombre: 'Nutricion',
+            grado: '1',
+            universidad: 'UNLAM'
+        })
+   
+     } catch(err){
+       console.log(err)
+     }
 })
