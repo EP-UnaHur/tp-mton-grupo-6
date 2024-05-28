192182e611a60dba20fdf5dcd55e50e806863d5b
MaauroSaavedra <mauro.saavedra@estudiantes.unahur.edu.ar>
2024-05-28 15:59:56 -0300

    base de datos conectada

--- src/app.js
+++ src/app.js
@@ -46,14 +46,18 @@ app.post('/carreras/:id/materia', async (req, res)=>{
     }
 })
 
-app.put('/carreras/:id', (req, res)=>{
+app.get('/carreras/:id/materias', async (req, res)=>{
     const id = req.params.id;
-    const idx = data.findIndex(e => e.id == id)
-    if (idx >= 0) {
-        data[idx] = {id: Number(id), ...req.body}
-        res.status(200).json(data[idx])
-    } else
-        res.status(404).json({error: `el id ${id} no existe`})
+    const carrera = await db.Carrera.findOne(
+        {
+            where: {id},
+            attributes:['id', 'nombre', 'grado', 'universidad']
+        })
+    const materias = await db.Materia.findAll({
+        where: {carreraId:id},
+        attributes:['id', 'nombre', 'cuatrimestral', 'anio']
+    });
+    res.status(200).json({carrera, materias})
 })
 
 app.get('/materias', async (req, res)=>{
@@ -66,19 +70,6 @@ app.listen(3000, async ()=>{
     try {
         await db.sequelize.authenticate()
         await db.sequelize.sync({force:true});
-   
-        db.Carrera.create({
-            nombre: 'Informatica',
-            grado: '3',
-            universidad: 'UNAHUR'
-        })
-   
-        db.Carrera.create({
-            nombre: 'Nutricion',
-            grado: '1',
-            universidad: 'UNLAM'
-        })
-   
      } catch(err){
        console.log(err)
      }
