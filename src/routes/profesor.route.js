41719431e519a9ec55dcef8d2cd2bd82c86550d4
MaauroSaavedra <mauro.saavedra@estudiantes.unahur.edu.ar>
2024-05-29 19:16:07 -0300

    creo endpoinds de profesores

--- src/routes/profesor.route.js
+++ src/routes/profesor.route.js
@@ -4,23 +4,55 @@ const db = require('../db/models')
 const route = Router()
 
 route.get('/profesores', async (req, res)=>{
-
+    const profesores = await db.Profesor.findAll({});
+    res.status(200).json(profesores)
 })
 
 route.get('/profesores/:id', async (req, res)=>{
-    
+    const id = req.params.id
+    const profesor = await db.Profesor.findOne(
+        {
+            where: {id},
+            attributes:['id', 'nombre', 'fechaNacimiento', 'legajo', 'activo']
+        })
+    if (profesor) 
+        res.status(200).json(profesor)
+    else res.status(404).json({error: `el id ${id} no existe`})
 })
 
 route.post('/profesores', async (req, res)=>{
-    
+    try {
+        const profesor = req.body
+        const newProfesor = await db.Profesor.create(profesor)
+        res.status(201).json(newProfesor)
+    } catch (err) {    
+        res.status(400).json(err.message)
+    }
 })
 
 route.put('/profesores/:id', async (req, res)=>{
-    
+    const idProfesor = req.params.id;
+    const newProfesor = db.Profesor.update(
+        req.body,
+        {
+            where: {id: idProfesor}
+        }
+    )
+    if (newProfesor) {
+        res.status(200).json(`El profesor con id ${idProfesor} se modifico con exito.`)
+    } else {
+        res.status(404).json({error: `El id ${idProfesor} no existe.`})
+    }
 })
 
 route.delete('/profesores/:id', async (req, res)=>{
-    
+    const id = req.params.id
+    const row = await db.Profesor.destroy({where: {id}})
+    if(row) {
+     res.status(200).json(`El profesor con id ${id} se borro con exito.`)
+    } else {
+     res.status(404).json(`El profesor con id ${id} no existe.`)
+    }
 })
 
 
