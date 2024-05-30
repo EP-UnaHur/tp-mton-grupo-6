89e978c7aa9b754ee528e9d15fd5c3acdc50641b
MaauroSaavedra <mauro.saavedra@estudiantes.unahur.edu.ar>
2024-05-29 23:26:03 -0300

    routes + controllers de curso

---
+++ src/controllers/curso.controller.js
@@ -0,0 +1,39 @@
+const {Curso} = require('../db/models')
+
+const controller = {}
+
+const getAllCursos = async (req, res)=>{
+    res.status(200).json(await Curso.findAll({}))
+}
+
+controller.getAllCursos = getAllCursos
+
+const cursoById = async (req, res)=>{
+    const id = req.params.id
+    res.status(200).json(await Curso.findByPk(id))
+}
+
+controller.cursoById = cursoById
+
+const eliminarCurso = async (req, res)=>{
+    const id = req.params.id
+    await Curso.destroy({where: {id}})
+    res.status(200).json(`El curso con id ${id} se elimino con exito`)
+}
+
+controller.eliminarCurso = eliminarCurso
+
+const modificarCurso = async (req, res)=>{
+    const id = req.params.id;
+    await Curso.update(
+        req.body,
+        {
+            where: {id}
+        }
+    )
+    res.status(200).json(`El curso con id ${id} se modifico con exito.`)
+}
+
+controller.modificarCurso = modificarCurso
+
+module.exports = controller
--- src/routes/curso.route.js
+++ src/routes/curso.route.js
@@ -1,50 +1,15 @@
 const {Router} = require('express')
-const db = require('../db/models');
-const { where } = require('sequelize');
+const cursoController = require('../controllers/curso.controller')
 
 const route = Router()
 
-route.get('/cursos', async (req, res)=>{
-    const cursos = await db.Curso.findAll({});
-    res.status(200).json(cursos)
-})
+route.get('/cursos', cursoController.getAllCursos)
 
-route.get('/cursos/:id', async (req, res)=>{
-    const id = req.params.id
-    const curso = await db.Curso.findOne(
-        {
-            where: {id},
-            attributes:['id', 'comision', 'turno', 'fechaInicio', 'fechaFin', 'materiaId']
-        })
-    if (curso) 
-        res.status(200).json(curso)
-    else res.status(404).json({error: `el id ${id} no existe`})
-})
+route.get('/cursos/:id', cursoController.cursoById)
 
-route.delete('/cursos/:id', async (req, res)=>{
-    const id = req.params.id
-    const row = await db.Curso.destroy({where: {id}})
-    if(row) {
-     res.status(200).json(`El curso con id ${id} se borro con exito.`)
-    } else {
-     res.status(404).json(`El curso con id ${id} no existe.`)
-    }
-})
+route.delete('/cursos/:id', cursoController.eliminarCurso)
 
-route.put('/cursos/:id', async (req, res)=>{
-    const idCurso = req.params.id;
-    const newCurso = db.Curso.update(
-        req.body,
-        {
-            where: {id: idCurso}
-        }
-    )
-    if (newCurso) {
-        res.status(200).json(`El curso con id ${idCurso} se modifico con exito.`)
-    } else {
-        res.status(404).json({error: `El id ${idCurso} no existe.`})
-    }
-})
+route.put('/cursos/:id', cursoController.modificarCurso)
 
 
 module.exports = route
