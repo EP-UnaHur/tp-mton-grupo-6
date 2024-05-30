6b5f4a6d374f82a4fcb393039cbde6a4742e7a9e
MaauroSaavedra <mauro.saavedra@estudiantes.unahur.edu.ar>
2024-05-30 13:57:42 -0300

    primer middleware

--- src/routes/carrera.route.js
+++ src/routes/carrera.route.js
@@ -1,11 +1,13 @@
 const {Router} = require('express')
+const {Carrera} = require('../db/models')
 const carreraController = require('../controllers/carrera.controller')
+const middleware = require('../middlewares/carrera.middleware')
 
 const route = Router()
 
 route.get('/carreras', carreraController.getAllCarreras)
 
-route.get('/carreras/:id', carreraController.carreraById)
+route.get('/carreras/:id', middleware.existsById(Carrera), carreraController.carreraById)
 
 route.post('/carreras', carreraController.crearCarrera)
 
